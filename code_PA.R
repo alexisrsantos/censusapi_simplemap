155c46c3e804ae9093bc5b047cd9e29a27fed2d8
Alexis R. Santos-Lozada <appdemography@gmail.com>
2018-09-13 03:49:17 -0400

    Add files via upload

---
+++ code_PA.R
@@ -0,0 +1,17 @@
+library(ggplot2) #Produces visual representations
+library(tidycensus) #Loads US Census files and data for "tidyverse" and "sf"
+library(tidyverse) #Supports "tidy" functions
+
+census_api_key('Your Census Key Here', install=TRUE, overwrite=TRUE) #Authenticates the access to the API
+
+vars<-c("B05001_001E","B05001_006E") #Number of Citizens and Total Pop by County
+
+penns <- get_acs(state = "PA", geography = "county", variables = vars, geometry = TRUE, output="wide")
+
+penns %>%
+  mutate(Percent = 100 * (B05001_006E / B05001_001E))%>%
+  ggplot(aes(fill = Percent, color = Percent)) + 
+  geom_sf() + 
+  coord_sf(crs = 2271)+labs(caption="ACS 2012-2016 ACS, by Alexis R. Santos",
+                            title="Percent non-citizen population by County in Pennsylvania",
+                            subtitle="Produced using the Census API and R")
---
+++ code_fill_in_the_blanks.R
@@ -0,0 +1,27 @@
+library(ggplot2) #Produces visual representations
+library(tidycensus) #Loads US Census files and data for "tidyverse" and "sf"
+library(tidyverse) #Supports "tidy" functions
+
+census_api_key('Your Census Key Here', install=TRUE, overwrite=TRUE) #Authenticates the access to the API
+
+# You can find a list of variables here: https://api.census.gov/data/2016/acs/acs5/variables.html
+vars<-c("Count of Interest","Total from the same table") 
+
+data <- get_acs(state = "State Abbreviation", geography = " See link below", variables = vars, geometry = TRUE, output="wide")
+
+#State Abbreviations: https://www.50states.com/abbreviations.htm
+#Geography: https://walkerke.github.io/tidycensus/articles/basic-usage.html#geography-in-tidycensus
+
+#Here we start the production of the map
+#Mutate Calculates the Percent of Interest
+#ggplot is used to produce the map
+#coord_sf is used to tell ggplot the state you are using following the EPSG Format
+#ESPG Codes are available here: http://spatialreference.org/ref/epsg/ (in the search bar write the name of your state)
+data %>%
+  mutate(Percent = 100 * (Population Count/Total from the same Table))%>%
+  ggplot(aes(fill = Percent, color = Percent)) + 
+  geom_sf() + 
+  coord_sf(crs =ESPG Code Here)+labs(caption="Caption Here",
+                            title="Title Here",
+                            subtitle="Subtitle here")
+
